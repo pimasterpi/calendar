d89ed023e9c136517f3127a551134f4767a71343
pimasterpi <lucas.indy@gmail.com>
2023-08-15 14:32:26 -0700

    Add date_time.h File & Reorganize Functions

--- events.h
+++ events.h
@@ -1,26 +1,11 @@
 //
-// Created by pimasterpi on 8/10/2023.
+// Last Modified by pimasterpi on 8/15/2023.
 //
+
+#include "date_time.h"
 
 #ifndef CALENDAR_EVENTS_H
 #define CALENDAR_EVENTS_H
-
-
-typedef struct time_struct {
-    int hour;
-    int minute;
-} time_struct_t;
-
-typedef struct date_struct {
-    int year;
-    int month;
-    int day;
-} date_struct_t;
-
-typedef struct date_time_struct {
-    date_struct_t date;
-    time_struct_t time;
-} date_time_struct_t;
 
 typedef struct event {
     char *name;
@@ -33,5 +18,11 @@ typedef struct event_list {
     struct event_list *prev;
     struct event_list *next;
 } event_list_t;
+
+typedef struct event_tree {
+    event_t event_info;
+    struct event_list *left;
+    struct event_list *right;
+} event_tree_t;
 
 #endif //CALENDAR_EVENTS_H
